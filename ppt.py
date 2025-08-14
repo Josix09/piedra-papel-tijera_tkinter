2143b713cd39a010aba84a24f11ee583e4373c3f
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-08-14 18:25:05 -0500

    up

--- ppt.py
+++ ppt.py
@@ -1,7 +1,15 @@
 # se importa la libreria tkinter con todas sus funciones
 from tkinter import *
+from tkinter import messagebox
+import random 
+
 
 Ventana_principal = Tk()
+    
+def salir():
+    messagebox.showinfo("Suma 1.0","La app se cerrará")
+    Ventana_principal.destroy()
+ 
 
 #Titulo de la ventana
 
@@ -34,7 +42,7 @@ frame_3 = Frame(Ventana_principal)
 frame_3.config(bg="ivory2",width=780, height=125)
 frame_3.place(x=10,y=370)
 
-titulo = Label(frame_1, text="¡Piedra Papel o tijeras!")
+titulo = Label(frame_1, text="¡Piedra Papel o Tijeras!")
 titulo.config(bg="gray", fg="white", font=("Arial", 18))
 titulo.place(x=260, y=10)
 
@@ -42,13 +50,37 @@ subtitulo1= Label(frame_1, text= "¡Juega contra la computadora!")
 subtitulo1.config(bg="gray",fg="white", font=("Arial",12))
 subtitulo1.place(x=270,y=43)
 
+#logo
+
+logo=PhotoImage(file="/home/sistemas/Documentos/Ejercicios_tkinter/piedra-papel-tijera_tkinter/Img/logodeljuego (1).png")
+label_logo = Label(frame_1, image=logo)
+label_logo.place(x=330,y=80)
+
+
+#   botones
+img_bt_papel= PhotoImage(file="/home/sistemas/Documentos/Ejercicios_tkinter/piedra-papel-tijera_tkinter/Img/papel.png")
+bt_papel=Button(frame_2,image=img_bt_papel, width=105,height=105)
+bt_papel.place(x=130,y=7)
+
+img_bt_piedra= PhotoImage(file="/home/sistemas/Documentos/Ejercicios_tkinter/piedra-papel-tijera_tkinter/Img/piedra.png")
+bt_piedra=Button(frame_2,image=img_bt_piedra, width=105,height=105)
+bt_piedra.place(x=330,y=7)
+
+img_bt_tijera= PhotoImage(file="/home/sistemas/Documentos/Ejercicios_tkinter/piedra-papel-tijera_tkinter/Img/images.png")
+bt_tijera=Button(frame_2,image=img_bt_tijera, width=105,height=105)
+bt_tijera.place(x=530,y=7)
+
+#boton salir
+img_bt_salir= PhotoImage(file="/home/sistemas/Documentos/Ejercicios_tkinter/piedra-papel-tijera_tkinter/Img/boton_salir.png")
+bt_salir=Button(frame_3,image=img_bt_salir, width=105,height=105,command=salir)
+bt_salir.place(x=530,y=7)  
 
 
 
 
 
 
-
+#Codigo del piedra papel o tijera mezclado con la interfaz:
 
 
 
